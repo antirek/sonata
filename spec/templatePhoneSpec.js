c9115a67926185106b9499977a0ed64d0b5104ea
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-02-19 21:22:29 +0700

    Добавил тест на включенный/выключенный аккаунт

--- spec/templatePhoneSpec.js
+++ spec/templatePhoneSpec.js
@@ -89,3 +89,48 @@ describe('template', () => {
     done();
   });
 });
+
+
+
+const grandstreamPhoneWithDisabledAccountsTemplate = {
+  id: 'sdgjdeu9443908590sfdsf8u984',
+  model: 'gxp1400',
+  vendor: 'grandstream',
+  mac: '001565113af8',
+  timezone_offset: 'GMT+03',
+  ntp_server: 'pool.ntp.org',
+  accounts: [
+    {
+      name: 'манго',
+      line: 1,
+      sip_register: 'sip.mangosip.ru',
+      sip_name: 'sip101',
+      sip_user: 'sip101',
+      sip_password: '1234',
+      sip_auth: 'sip101',
+      enabled: false,
+    },
+    {
+      name: 'Мобилон',
+      line: 2,
+      sip_register: 'sip.mobilonsip.ru',
+      sip_name: 'sip102',
+      sip_user: 'sip102',
+      sip_password: '4321',
+      sip_auth: 'sip102',
+      enabled: true,
+    },
+  ],
+};
+
+
+describe('template', () => {
+  it('do good with enabled and disabled accounts', (done) => {
+    const config = template(grandstreamPhoneWithDisabledAccountsTemplate);
+    // console.log('config:', config);
+    expect(config.includes('<P271>0</P271>')).toBe(true);
+    expect(config.includes('<P401>1</P401>')).toBe(true);
+
+    done();
+  });
+});
