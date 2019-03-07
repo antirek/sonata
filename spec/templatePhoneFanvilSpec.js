009fa045bdbc1ac337423f1dedbdc4c15c37c363
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-03-08 00:14:15 +0700

    добавил в шаблон fanvil разметку для 6 учеток

---
+++ spec/templatePhoneFanvilSpec.js
@@ -0,0 +1,51 @@
+const template = require('./../provision/template');
+
+const panasonicPhone = {
+  id: 'sdgjdeu9443908590sfdsf8u984',
+  model: 'x5s',
+  vendor: 'fanvil',
+  mac: '001565113af8',
+  timezone_offset: 'GMT+03',
+  ntp_server: 'pool.ntp.org',
+  accounts: [
+    {
+      name: 'манго',
+      line: 1,
+      sip_register: 'sip.mangosip.ru',
+      sip_name: 'sip101',
+      sip_user: 'sip101',
+      sip_password: '1234',
+      sip_auth: 'sip101',
+    },
+    {
+      name: 'Мобилон',
+      line: 2,
+      sip_register: 'sip.mobilonsip.ru',
+      sip_name: 'sip102',
+      sip_user: 'sip102',
+      sip_password: '4321',
+      sip_auth: 'sip102',
+    },
+  ],
+};
+
+
+describe('template', () => {
+  it('do good', (done) => {
+    const config = template(panasonicPhone);
+    console.log('config:', config);
+
+    expect(config.includes('SIP1 Phone Number  :sip101')).toBe(true);
+    expect(config.includes('SIP1 Register Pswd :1234')).toBe(true);
+    expect(config.includes('SIP1 Register Addr :sip.mangosip.ru')).toBe(true);
+
+    expect(config.includes('SIP2 Phone Number  :sip102')).toBe(true);
+    expect(config.includes('SIP2 Register Pswd :4321')).toBe(true);
+    expect(config.includes('SIP2 Register Addr :sip.mobilonsip.ru')).toBe(true);
+
+    //expect(config.includes('TIME_ZONE="180"')).toBe(true);
+
+    done();
+  });
+});
+
