41cc0b46395708923fe7cb2523d11b48412edb72
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-03-08 16:16:52 +0700

    переименовал все существующие конфиги в дефолтные при необходимости для особой модели можно добавить отдельный конфиг

--- provision/vendors/index.js
+++ provision/vendors/index.js
@@ -3,34 +3,34 @@ module.exports = {
   grandstream: {
     gxp1400: {
       type: 'phone',
-      template: 'gxp1400.xml',
+      template: 'default_phone.xml',
       accounts: 2,
     },
     gxp1610: {
       type: 'phone',
-      template: 'gxp1610.xml',
+      template: 'default_phone.xml',
       accounts: 1,
     },
     gxp1620: {
       type: 'phone',
-      template: 'gxp1620.xml',
+      template: 'default_phone.xml',
       accounts: 2,
     },
     dp715: {
       type: 'gateway',
-      template: 'dp715.xml',
+      template: 'default_gateway.xml',
       accounts: 5,
       profiles: 2,
     },
     ht812: {
       type: 'gateway',
-      template: 'ht812.xml',
+      template: 'default_gateway.xml',
       accounts: 2,
       profiles: 2,
     },
     ht814: {
       type: 'gateway',
-      template: 'ht814.xml',
+      template: 'default_gateway.xml',
       accounts: 4,
       profiles: 2,
     },
@@ -38,25 +38,25 @@ module.exports = {
   panasonic: {
     tgp600: {
       type: 'phone',
-      template: 'tgp600.sys',
+      template: 'default.sys',
       accounts: 8,
     },
   },
   yealink: {
     t28p: {
       type: 'phone',
-      template: 't28p.cfg',
+      template: 'default.cfg',
       accounts: 6,
     },
   },
   fanvil: {
     c58: {
       type: 'phone',
-      template: 'c58.cfg',
+      template: 'default.cfg',
     },
     x5s: {
       type: 'phone',
-      template: 'c58.cfg',
+      template: 'default.cfg',
       accounts: 6,
     },
   },
