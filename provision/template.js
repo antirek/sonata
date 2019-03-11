30ac6b4a46d3820cdcf2c0d7ab1c5a7d9ecb2cb8
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-03-11 21:55:51 +0700

    вынес таймзоны в отдельный файл добавил известные коды таймзон для fanvil

--- provision/template.js
+++ provision/template.js
@@ -3,53 +3,8 @@ const path = require('path');
 const devices = require('./vendors/index');
 const preprocess = require('preprocess');
 const url = require('url-parse');
+const timezones = require('./timezones');
 
-
-const getTimezoneByOffset = (offset, vendor) => {
-  const offsets = {
-    'grandstream': {
-      'GMT+01': 'CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00',
-      'GMT+02': 'TZP-2',
-      'GMT+03': 'TZQ-3',
-      'GMT+04': 'TZR-4',
-      'GMT+05': 'TZS-5',
-      'GMT+06': 'TZV-6',
-      'GMT+07': 'TZX-7',
-      'GMT+08': 'TZY-8',
-      'GMT+09': 'TZZ-9',
-      'GMT+10': 'EST-10',
-      'GMT+11': 'TZc-11',
-    },
-    'yealink': {
-      'GMT+01': '+1',
-      'GMT+02': '+2',
-      'GMT+03': '+3',
-      'GMT+04': '+4',
-      'GMT+05': '+5',
-      'GMT+06': '+6',
-      'GMT+07': '+7',
-      'GMT+08': '+8',
-      'GMT+09': '+9',
-      'GMT+10': '+10',
-      'GMT+11': '+11',
-    },
-    'panasonic': {
-      'GMT+01': '60',
-      'GMT+02': '120',
-      'GMT+03': '180',
-      'GMT+04': '240',
-      'GMT+05': '300',
-      'GMT+06': '360',
-      'GMT+07': '420',
-      'GMT+08': '480',
-      'GMT+09': '540',
-      'GMT+10': '600',
-      'GMT+11': '660',
-    },
-  };
-
-  return offsets[vendor] ? offsets[vendor][offset] : null;
-};
 
 const replacePhonebooksVars = (config, phonebooks) => {
   phonebooks.forEach((element, id) => {
@@ -99,7 +54,7 @@ const replaceAccountsVars = (config, accounts) => {
 const phoneReplace = (template, device) => {
   let config = template.toString('utf8')
       .replace('{{timezone}}',
-          getTimezoneByOffset(device.timezone_offset, device.vendor))
+          timezones.getTimezoneByOffset(device.timezone_offset, device.vendor))
       .replace('{{ntp_server}}', device.ntp_server)
       .replace(/<!--[\s\S]*?-->/g, '')
       .replace(/\n\n/g, '\n');
---
+++ provision/timezones.js
@@ -0,0 +1,63 @@
+
+const getTimezoneByOffset = (offset, vendor) => {
+const offsets = {
+  'grandstream': {
+    'GMT+01': 'CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00',
+    'GMT+02': 'TZP-2',
+    'GMT+03': 'TZQ-3',
+    'GMT+04': 'TZR-4',
+    'GMT+05': 'TZS-5',
+    'GMT+06': 'TZV-6',
+    'GMT+07': 'TZX-7',
+    'GMT+08': 'TZY-8',
+    'GMT+09': 'TZZ-9',
+    'GMT+10': 'EST-10',
+    'GMT+11': 'TZc-11',
+  },
+  'yealink': {
+    'GMT+01': '+1',
+    'GMT+02': '+2',
+    'GMT+03': '+3',
+    'GMT+04': '+4',
+    'GMT+05': '+5',
+    'GMT+06': '+6',
+    'GMT+07': '+7',
+    'GMT+08': '+8',
+    'GMT+09': '+9',
+    'GMT+10': '+10',
+    'GMT+11': '+11',
+    },
+    'panasonic': {
+    'GMT+01': '60',
+    'GMT+02': '120',
+    'GMT+03': '180',
+    'GMT+04': '240',
+    'GMT+05': '300',
+    'GMT+06': '360',
+    'GMT+07': '420',
+    'GMT+08': '480',
+    'GMT+09': '540',
+    'GMT+10': '600',
+    'GMT+11': '660',
+    },
+    'fanvil': {
+    'GMT+01': '',
+    'GMT+02': '',
+    'GMT+03': '12',
+    'GMT+04': '',
+    'GMT+05': '',
+    'GMT+06': '',
+    'GMT+07': '28',
+    'GMT+08': '',
+    'GMT+09': '',
+    'GMT+10': '',
+    'GMT+11': '',
+    },
+  };
+
+  return offsets[vendor] ? offsets[vendor][offset] : null;
+};
+
+module.exports = {
+  getTimezoneByOffset
+}
