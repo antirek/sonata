4a7d02407c106127af423df5d8bd669a5ccfed58
Tolyan <k666r6@gmail.com>
2019-03-12 09:53:56 +0700

    Добавил сортировку и лимит с офсетом при запросах в

--- manage/api-routes/log/request/{token}.js
+++ manage/api-routes/log/request/{token}.js
@@ -8,8 +8,15 @@ module.exports = (RequestLog) => {
   const get = (req, res) => {
     console.log('request params', req.params);
     console.log('request body:', JSON.stringify(req.body));
+    console.log('request query', req.query);
+    const limit = req.query.limit || 10;
+    const offset = req.query.offset || 0;
 
     RequestLog.find({token: req.params.token})
+        .skip(parseInt(offset))
+        .limit(parseInt(limit))
+        .sort({created_at: -1})
+        .exec()
         .then((list) => {
           console.log('vendors list:', list);
           res.status(200).json(list);
@@ -49,6 +56,18 @@ module.exports = (RequestLog) => {
         type: 'string',
         required: true,
         description: 'token',
+      },
+      {
+        name: 'limit',
+        in: 'query',
+        type: 'integer',
+        description: 'limit',
+      },
+      {
+        name: 'offset',
+        in: 'query',
+        type: 'integer',
+        description: 'offset',
       },
     ],
     get,
